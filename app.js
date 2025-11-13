e59d191814e563275f0fe47771755845331fc399
KMJ <kmj13520@naver.com>
2025-11-13 15:32:39 +0900

    Js추가

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert("테스트");
+function changeColor() {
+  document.body.style.backgroundColor =
+    "rgb(" +
+    Math.round(Math.random() * 255) +
+    "," +
+    Math.round(Math.random() * 255) +
+    "," +
+    Math.round(Math.random() * 255) +
+    ")";
+}
